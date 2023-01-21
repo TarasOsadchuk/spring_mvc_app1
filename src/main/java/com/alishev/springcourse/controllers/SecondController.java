144fada6791a66cf2c8743b0e653606b0a520e08
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-01-21 17:28:59 +0200

    GET, @RequestParam

---
+++ src/main/java/com/alishev/springcourse/controllers/SecondController.java
@@ -0,0 +1,15 @@
+package com.alishev.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/second")
+public class SecondController {
+
+    @GetMapping("/exit")
+    public String exit() {
+        return "second/exit";
+    }
+}
