9a480e93ef1b81a9d5981526a452b3bce2468f68
Taras Osadchuk <tarasosadchuk.v@gmail.com>
2023-01-21 21:38:31 +0200

    Calculator

--- src/main/java/com/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/com/alishev/springcourse/controllers/FirstController.java
@@ -1,11 +1,10 @@
 package com.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
@@ -13,11 +12,11 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-//        String name = request.getParameter("name");
-//        String surname = request.getParameter("surname");
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname + "!");
+//        System.out.println("Hello, " + name + " " + surname + "!");
+        model.addAttribute("message", "Hello, " + name + " " + surname + "!");
 
         return "first/hello";
     }
@@ -25,5 +24,25 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String helloPage(@RequestParam("a") int a,
+                            @RequestParam("b") int b,
+                            @RequestParam("action") String action,
+                            Model model) {
+
+        if (action.equals("multiplication")) {
+            model.addAttribute("result", a + " * " + b + " = " + (a * b));
+        } else if (action.equals("addition")) {
+            model.addAttribute("result", a + " + " + b + " = " + (a + b));
+        } else if (action.equals("subtraction")) {
+            model.addAttribute("result", a + " - " + b + " = " + (a - b));
+        } else if (action.equals("division")) {
+            model.addAttribute("result", a + " / " + b + " = " + (a / (double)b));
+        } else {
+            model.addAttribute("result", "Enter the correct data");
+        }
+        return "first/calculator";
     }
 }
